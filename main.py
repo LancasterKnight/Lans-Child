4240a7000a3f83b22f64dd37dc9fd52566fb0e99
LancasterKnight <matthew.spiteri1997@gmail.com>
2025-07-25 11:21:10 +0200

    1.3.14.2

--- main.py
+++ main.py
@@ -346,6 +346,24 @@ async def on_message(message):
         await message.channel.send(random.choice(salem_response))
 #---
 #---
+    joe_trigger = [r"/bjoe/b"]
+    joe_response = ["""Who’s joe?" a distant voice asks.
+
+Instantly everyone nearby hears the sound of 1,000s of bricks rapidly shuffling towards his location.
+
+The earth itself seemed to cry out in agony, until finally the ground itself split open and a horrific creature crawled from the ground, covered in mucus and tar.
+
+”Joe Momma…” the creature whispered.
+
+The man cried out in pain as he disintegrated into dust, and the whole world fell silent in fear.
+
+"I did a little trolling." the wretched creature remarked before burrowing back into the earth."""
+    ]
+
+    if any(phrase in message.content.lower() for phrase in joe_trigger):
+        await message.channel.send(random.choice(joe_response))
+#---
+#---
     clanker_trigger = ["clanker"]
     clanker_response = """WEE WOO WEE WOO
     
